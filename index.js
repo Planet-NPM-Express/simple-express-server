fcb39797b6e75b35b9fd0be0a615e66dc22e3c3c
Charlie <boy_916@hotmail.com>
2021-12-01 21:24:27 -0800

    added readme

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 function startServer(port, lifeProof) {
   const express = require('express');
-  
   const app = express()
-  // const port = 3000
-  // let lifeProof = 'Wazzup!';
   
   app.get('/', (req, res) => {
     res.send(`${lifeProof}`);
   });
   
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Simple Express Server 1239 listening at http://localhost:${port}`)
   });
-
 }
 
 module.exports = startServer;
