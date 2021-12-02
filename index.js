e450dc7941b479cc9a9bfbf13ae1d5114b34dd39
Charlie <boy_916@hotmail.com>
2021-12-01 21:07:47 -0800

    updating version

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+function startServer(port, lifeProof) {
+  const express = require('express');
+  
+  const app = express()
+  // const port = 3000
+  // let lifeProof = 'Wazzup!';
+  
+  app.get('/', (req, res) => {
+    res.send(`${lifeProof}`);
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = startServer;
