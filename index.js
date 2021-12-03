2ec220b7558568bc89aad5d0927fd0092b18decc
Charlie <boy_916@hotmail.com>
2021-12-02 19:16:36 -0800

    publishing fixed code

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 
-// function startServer(port, lifeProof) {
-//   const express = require('express');
-//   const app = express()
+function startServer(port, lifeProof) {
+  const express = require('express');
+  const app = express()
   
-//   app.get('/', (req, res) => {
-//     res.send(`${lifeProof}`);
-//   });
+  app.get('/', (req, res) => {
+    res.send(`${lifeProof}`);
+  });
   
-//   app.listen(port, () => {
-//     console.log(`Simple Express Server 1239 listening at http://localhost:${port}`)
-//   });
-// }
-
-// module.exports = startServer;
-
-class expressServer {
-  startServer(port, lifeProof) {
-    const express = require('express');
-    const app = express()
-    
-    app.get('/', (req, res) => {
-      res.send(`${lifeProof}`);
-    });
-    
-    app.listen(port, () => {
-      console.log(`Simple Express Server 1239 listening at http://localhost:${port}`)
-    });
-  }
+  app.listen(port, () => {
+    console.log(`Simple Express Server 1239 listening at http://localhost:${port}`)
+  });
 }
 
-module.exports = expressServer;
+module.exports = startServer;
+
+// class expressServer {
+//   startServer(port, lifeProof) {
+//     const express = require('express');
+//     const app = express()
+    
+//     app.get('/', (req, res) => {
+//       res.send(`${lifeProof}`);
+//     });
+    
+//     app.listen(port, () => {
+//       console.log(`Simple Express Server 1239 listening at http://localhost:${port}`)
+//     });
+//   }
+// }
+
+// module.exports = expressServer;
